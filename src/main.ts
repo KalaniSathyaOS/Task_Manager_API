e2b3c537c951eac793323bf9f4bd7aac8877da9b
Kalani Sathya <ksathya@opiksoft.com>
2026-05-18 14:58:42 +0530

    setup Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import open from 'open';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+
+const config = new DocumentBuilder()
+  .setTitle('Task Manager API')
+  .setVersion('1.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    },
+    'access-token', 
+  )
+    .addSecurityRequirements('access-token')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+
+  await open('http://localhost:3000/api');
+
 }
 bootstrap();
